fb723cbd90750f395eb3fcea91b46a26f72c7051
almir-pehlivanovic <pehlivanovicalmir1@gmail.com>
2023-10-23 21:05:07 +0200

    Initial setup

---
+++ tailwind-config.js
@@ -0,0 +1,60 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif']
+      },
+      colors: {
+        grey: {
+          lawi: {
+            '10': "#D9D9D9",
+            '20': '#ECECEC',
+            '300': "#64697A",
+            '400': "#424242",
+            '450': '#888888',
+            '500': '#3e6855',
+            '550': '#DFDCDC',
+          }
+        },
+        hgMenu: {
+          '50': "#8F8F8F",
+          '100': "#333333",
+          '250': "#ECF0F1",
+        },
+        custom: {
+          100: "#E5E6E8",
+        },
+        footer:{
+          100: "#3F3F46",
+        },
+        blue:{
+          'lawi-bright': {
+              100: "#136597",
+          }
+        },
+        orange:{
+          lawi:{
+            100: "#F29400",
+          }
+        },
+        green: {
+          lawi: {
+            '10': "#60B565",
+            '100': '#008a4c',
+          }
+        },
+        red: {
+          lawi: {
+            '100': '#BD3305'
+          }
+        },
+        dark: {
+          lawi:{
+            50: "#111826",
+          }
+        },
+        hell: '#F5F5F5',
+      }
+    }
+  }
+}
